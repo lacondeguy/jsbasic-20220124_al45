4624caed2927b8f24f8600278efd3465b339d66c
Lacondeguy <ka4alin.alex@gmail.com>
2022-01-27 23:48:12 +0300

    Занятие 1, задача 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+   if (n == 0 || n == 1) {
+      return 1;
+   } else {
+     let result = n
+         while (n > 1) {
+         --n;
+         result *= n;
+         } 
+     return result;
+   }
 }
