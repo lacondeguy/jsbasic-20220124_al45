11d0d73d2112f42544131582d8eddf72eeeb9aeb
Lacondeguy <ka4alin.alex@gmail.com>
2022-02-05 02:30:07 +0300

    Занятие 3, задача 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+ let zarplata = 0;
+ 
+ for (key in salaries) {
+     if (isFinite(salaries[key])) {
+     zarplata += salaries[key];
+     } 
+  } 
+  return zarplata;
 }
