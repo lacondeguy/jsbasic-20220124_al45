6c465b142a2b3c35c6c4b627179175565e47652f
Lacondeguy <ka4alin.alex@gmail.com>
2022-02-07 16:58:54 +0300

    Занятие 3, задача 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -6,21 +6,9 @@ for (let i = 0; i < result.length; i++){
    
      if (i > 0) {
      result[i] = (result[i].slice(0, result[i].length - result[i].length + 1)).toUpperCase() + result[i].slice(1, result[i].length);
-     };
-
-
-     
+     };    
 };
 
 result = result.join('');
-
 return result;
 }
-
-
-
-
-
-console.log(camelize('background-color'));
-console.log(camelize('list-style-image'));
-console.log(camelize('-webkit-transition'));
