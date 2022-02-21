01d74c3bdfe5c37983ea17077f1188f86e970343
Lacondeguy <ka4alin.alex@gmail.com>
2022-02-21 16:39:24 +0300

    Задачи до 5-2

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+
+ for (let i = 0; i < table.rows.length; i++) {
+
+   let tr = table.rows[i];
+   let status = tr.cells[3].getAttribute('data-available');
+   let gender = tr.cells[2].textContent;
+   let age = tr.cells[1].textContent;
+  
+
+
+    if (status == "true") {
+    tr.classList.add('available');
+    } else if (status == "false") {
+    tr.classList.add('unavailable');
+    } else if (tr.cells[3].hasAttribute('data-available') == "") {
+    tr.setAttribute('hidden', "");
+    }
+    
+    if (gender === "m") {
+    tr.classList.add('male');
+    } else if (gender === "f") {
+    tr.classList.add('female');
+    }
+     
+    if (age < 18 ) {
+    tr.setAttribute('style', 'text-decoration: line-through');
+    }
+
+ }
 }
