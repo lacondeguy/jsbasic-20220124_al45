0e4aa630eb0ea2c9b8bb7956f17ca815990ff34a
Lacondeguy <ka4alin.alex@gmail.com>
2022-02-28 16:59:50 +0300

    Задача 5-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,43 +1,43 @@
 function initCarousel() {
 
-let carouselSlides = document.querySelector('.carousel__inner');
-let leftButton = document.querySelector('.carousel__arrow_left');
-let rightButton = document.querySelector('.carousel__arrow_right');
-let size = carouselSlides.offsetWidth;
-let coordinates = 0;
-let currentSlide = 1;
-
-leftButton.style.display = 'none';
-
-rightButton.addEventListener('click', () => {
+  let carouselSlides = document.querySelector('.carousel__inner');
+  let leftButton = document.querySelector('.carousel__arrow_left');
+  let rightButton = document.querySelector('.carousel__arrow_right');
+  let size = carouselSlides.offsetWidth;
+  let coordinates = 0;
+  let currentSlide = 1;
   
-  currentSlide += 1;
-  coordinates = coordinates + size;
-  carouselSlides.style.transform = 'translateX(-' + coordinates + 'px)';
-
-  if (currentSlide == 4){
-  rightButton.style.display = 'none';
-  } else {
-  rightButton.style.display = '';
-  leftButton.style.display = '';
-  }
-
-});
-
-
-leftButton.addEventListener('click', () => {
-  
-  currentSlide = currentSlide - 1;
-  coordinates = coordinates - size;
-  carouselSlides.style.transform = 'translateX(-' + coordinates + 'px)';
-
-  if (currentSlide == 1) {
   leftButton.style.display = 'none';
-  } else {
-  rightButton.style.display = '';
-  leftButton.style.display = '';
+  
+  rightButton.addEventListener('click', () => {
+    
+    currentSlide += 1;
+    coordinates = coordinates + size;
+    carouselSlides.style.transform = 'translateX(-' + coordinates + 'px)';
+  
+    if (currentSlide == 4){
+    rightButton.style.display = 'none';
+    } else {
+    rightButton.style.display = '';
+    leftButton.style.display = '';
+    }
+  
+  });
+  
+  
+  leftButton.addEventListener('click', () => {
+    
+    currentSlide = currentSlide - 1;
+    coordinates = coordinates - size;
+    carouselSlides.style.transform = 'translateX(-' + coordinates + 'px)';
+  
+    if (currentSlide == 1) {
+    leftButton.style.display = 'none';
+    } else {
+    rightButton.style.display = '';
+    leftButton.style.display = '';
+    }
+  
+  });
+  
   }
-
-});
-
-}
